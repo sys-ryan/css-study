5b45c0c125564d859ef6b4fbfb917bca3b401cc2
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-03-01 16:45:07 +0900

    create modal close feature

--- 07-Javascript_and_CSS/01-adding-a-modal/shared.js
+++ 07-Javascript_and_CSS/01-adding-a-modal/shared.js
@@ -1,5 +1,6 @@
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
+var modalNoButtons = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 
 selectPlanButtons.forEach((btn) => {
@@ -8,3 +9,11 @@ selectPlanButtons.forEach((btn) => {
     backdrop.style.display = "block";
   });
 });
+
+const closeModal = () => {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+};
+
+backdrop.addEventListener("click", closeModal);
+modalNoButtons.addEventListener("click", closeModal);
