f86a2d9b3545a593fec9cf5b50f773b44a2c113a
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-03-01 17:05:47 +0900

    opening and closing the hamburger menu

--- 07-Javascript_and_CSS/01-adding-a-modal/shared.js
+++ 07-Javascript_and_CSS/01-adding-a-modal/shared.js
@@ -3,6 +3,9 @@ var modal = document.querySelector(".modal");
 var modalNoButtons = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
 selectPlanButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
     modal.style.display = "block";
@@ -15,5 +18,13 @@ const closeModal = () => {
   backdrop.style.display = "none";
 };
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", () => {
+  mobileNav.style.display = "none";
+  backdrop.style.display = "none";
+});
 modalNoButtons.addEventListener("click", closeModal);
+
+toggleButton.addEventListener("click", () => {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
