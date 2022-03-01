42fdfef9e4d3204d859c1b4bb555ae6001839ba3
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-03-01 17:13:18 +0900

    Manipulating Element Classes
    - Element.classList.(function)
    -- add
    -- contains
    -- item
    -- rermove
    -- toggle
    -- toString

--- 07-Javascript_and_CSS/01-adding-a-modal/shared.js
+++ 07-Javascript_and_CSS/01-adding-a-modal/shared.js
@@ -8,23 +8,31 @@ var mobileNav = document.querySelector(".mobile-nav");
 
 selectPlanButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open' //this will actually overwrite the complete class list
+    modal.classList.add("open");
+    backdrop.classList.add("open");
   });
 });
 
 const closeModal = () => {
-  modal.style.display = "none";
-  backdrop.style.display = "none";
+  // modal.style.display = "none";
+  // backdrop.style.display = "none";
+  modal.classList.remove("open");
+  backdrop.classList.remove("open");
 };
 
 backdrop.addEventListener("click", () => {
-  mobileNav.style.display = "none";
-  backdrop.style.display = "none";
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
+  closeModal();
 });
 modalNoButtons.addEventListener("click", closeModal);
 
 toggleButton.addEventListener("click", () => {
-  mobileNav.style.display = "block";
-  backdrop.style.display = "block";
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
 });
